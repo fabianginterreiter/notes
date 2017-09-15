6b0801a1fb478306150d99f0e28aece428f86fd4
Fabian Ginterreiter <fabian.ginterreiter@gmail.com>
2017-09-15 19:10:51 +0200

    Edit function to edit notes (#3)

--- client/components/Note.js
+++ client/components/Note.js
@@ -9,6 +9,7 @@ class Note extends React.Component {
     super(props);
 
     this.state = {
+      edit: null,
       note: null,
       style: this.createStyle(PanelsStore.getObject())
     }
@@ -17,7 +18,7 @@ class Note extends React.Component {
   componentDidMount() {
     this.componentWillReceiveProps(this.props);
 
-    PanelsStore.addChangeListener(this, (e) => 
+    PanelsStore.addChangeListener(this, (e) =>
       this.setState({
         style:this.createStyle(e)
       })
@@ -44,7 +45,6 @@ class Note extends React.Component {
 
   handleReload() {
     fetch('/api/reload').then(() => {
-      console.log("reload");
       ReloadListener.dispatch();
     });
   }
@@ -64,15 +64,54 @@ class Note extends React.Component {
       buttons.push(<span key="notes" onClick={() => PanelsStore.setNotes(true)}><i className="fa fa-file-text-o" />&nbsp;</span>);
     }
 
+    if (this.state.edit) {
+      buttons.push(<span key="cancel" onClick={() => this.setState({edit:null})}>Cancel</span>);
+      buttons.push(<span key="save" onClick={() => this.handleSave()}>Save</span>);
+    } else {
+      buttons.push(<span key="edit" onClick={() => this.setState({edit:this.state.note.content})}>Edit</span>);
+    }
+
+
     var content = null;
     if (this.state.note) {
-      content = (<div className="content"><ReactMarkdown source={this.state.note.content} /></div>);
+
+      if (this.state.edit) {
+        content =(<div className="content"><textarea value={this.state.edit} onChange={(event) => this.handleChange(event)} /></div>);
+      } else {
+        content = (<div className="content"><div className="text"><ReactMarkdown source={this.state.note.content} /></div></div>);
+      }
     }
 
     return (<div className="note" style={this.state.style}>
         <header>{buttons}&nbsp;<span onClick={this.handleReload.bind(this)} className="right"><i className="fa fa-refresh" /></span></header>
         {content}
       </div>)
+  }
+
+  handleChange(event) {
+    this.setState({
+      edit: event.target.value
+    });
+  }
+
+  handleSave() {
+    var note = this.state.note;
+    note.content = this.state.edit;
+
+    fetch('/api/note' + this.props.file, {
+      body: JSON.stringify(note),
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+      },
+      method: "PUT"
+    }).then((result) => result.json()).then((object) => {
+      ReloadListener.dispatch();
+      this.setState({
+        note: object,
+        edit: null
+      });
+    });
   }
 }
 
--- index.js
+++ index.js
@@ -1,59 +1,41 @@
 var fs = require("fs"),
     path = require("path"),
-    util = require("util");
-	
-var readFile = require('./lib/readFile');
-	
-var scan = require('./lib/scan');
+    Notes = require("./lib/Notes"),
+    bodyParser = require("body-parser");
 
-var Notes = require('./lib/Notes');
-	
-var notes = new Notes(path.join(__dirname, "files"));
+var config = JSON.parse(fs.readFileSync("config.json"));
+
+console.log("Root Path: " + config.path);
+
+var notes = new Notes(config.path);
 notes.init();
 
-var express = require('express');
+var express = require("express");
 var app = express();
 
-app.get('/api/all', function (req, res) {
-  res.send(notes.getData());
-});
+app.use(bodyParser.json({limit: "50mb"}));
+app.use(bodyParser.urlencoded({limit: "50mb", extended: true}));
 
-app.get('/api/tags*', (req, res) => {
-  res.send(notes.getTags(req.params[0], req.query.tags));
-});
+app.get("/api/all", (req, res) => res.send(notes.getData()));
+app.get("/api/tags*", (req, res) => res.send(notes.getTags(req.params[0], req.query.tags)));
+app.get("/api/categories*", (req, res) => res.send(notes.getCategories(req.params[0])));
+app.get("/api/notes*", (req, res) => res.send(notes.getNotes(req.params[0], req.query.tags)));
+app.get("/api/note/*", (req, res) => notes.getNote(req.params[0]).then((file) => res.send(file)));
+app.put("/api/note/*", (req, res) => notes.updateNote(req.params[0], req.body).then((file) => res.send(file)));
+app.get("/api/reload", (req, res) => notes.init().then(() => res.status(200).send("OK")).catch((e) => res.status(400)));
 
-app.get('/api/categories*', (req, res) => {
-  res.send(notes.getCategories(req.params[0]));
-});
+app.use(express.static("public"));
 
-app.get('/api/notes*', (req, res) => {
-  res.send(notes.getNotes(req.params[0], req.query.tags));
-});
-
-app.get('/api/note/*', (req, res) => {
-  notes.getNote(req.params[0]).then((file) => res.send(file));
-});
-
-app.get('/api/reload', (req, res) => {
-  notes.init().then(() => res.status(200).send('OK')).catch((e) => res.status(400));
-});
-
-app.use(express.static('public'));
-
-app.use('*', (req, res, next) => {
-  if (req.params[0].endsWith('.md')) {
-    res.sendFile(path.resolve(__dirname, 'public', 'index.html'))  
+app.use("*", (req, res, next) => {
+  if (req.params[0].endsWith(".md")) {
+    res.sendFile(path.resolve(__dirname, "public", "index.html"))
   } else {
     next();
   }
 });
 
-app.use(express.static('files'));
+app.use(express.static(config.path));
 
-app.get('*', function(req, res) {
-  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
-});
+app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "public", "index.html")));
 
-app.listen(9000, function () {
-  console.log('Example app listening on port 9000!');
-});
+app.listen(9000, () => console.log("Example app listening on port 9000!"));
